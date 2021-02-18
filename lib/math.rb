7237321f6f90a23b0bb1264bb7534d349e038ab0
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-18 00:10:02 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  add = (num1 + num2)
+  return add
 end
 
 def subtraction(num1, num2)
-
+  subtraction = (num1 - num2)
+  return subtraction
 end
 
 def division(num1, num2)
-
+  division = (num1 / num2)
+  return division
 end
 
 def multiplication(num1, num2)
-
+  multiplication = (num1 * num2)
+  return multiplication
 end
 
 def modulo(num1, num2)
-
+  modulo = (num1 % num2)
+  return modulo
 end
 
 def square_root(num)
-
+  root = Math.sqrt(num)
 end
