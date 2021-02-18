313f596dc073f74d7c028fb83ee4cd8031d77c8d
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-18 00:03:27 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,22 +1,28 @@
-def addition(num1, num2)
 
+def addition(num1, num2)
+ return == #{num1 + num2}
 end
+
 
 def subtraction(num1, num2)
-
+ return #{num1 - num2}
 end
+
 
 def division(num1, num2)
-
+ return #{num1 / num2}
 end
+
 
 def multiplication(num1, num2)
-
+ return #{num1 * num2}
 end
+
 
 def modulo(num1, num2)
-
+ return #{num1 % num2}
 end
+
 
 def square_root(num)
 
